22634f4f025067d39808732345a21d59cbf20987
Natanael Carvalho Da Silva <161735922+NatanCarFF@users.noreply.github.com>
2024-04-26 18:14:56 +0000

    Colocando espaço entre os dados

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -5,7 +5,7 @@ dado1 = input("Digite o primeiro dado: ")
 dado2 = input("Digite o segundo dado: ")
 
 # Concatenando os dados em uma única string
-concatenado = dado1 + dado2
+concatenado = dado1 +  ' ' + dado2
 
 # Imprimindo o resultado
 print("Os dados concatenados são:", concatenado)

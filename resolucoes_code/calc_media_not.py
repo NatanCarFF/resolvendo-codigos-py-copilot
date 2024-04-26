a9ed2ace5de77f9dd9d1520a5370d2041c9154cb
Natanael Carvalho Da Silva <161735922+NatanCarFF@users.noreply.github.com>
2024-04-26 18:31:27 +0000

    Atualização 260420241531

---
+++ resolucoes_code/calc_media_not.py
@@ -0,0 +1,12 @@
+# Agora vamos calcular a média de três notas fornecidas na entrada do usuário. Uma dica é: Utilize operadores aritméticos para realizar o cálculo da média.
+
+# Solicitando as notas do usuário
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+# Calculando a média
+media = (nota1 + nota2 + nota3) / 3
+
+# Imprimindo o resultado
+print("A média das três notas é:", media)
